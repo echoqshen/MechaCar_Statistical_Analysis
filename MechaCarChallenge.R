fa9f95b30e2461089ecc297446e6b4cf32f8a28c
echoqshen <90880564+echoqshen@users.noreply.github.com>
2022-01-09 12:54:52 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,12 +5,18 @@ mechacar_df<-read.csv('MechaCar_mpg.csv', check.names = F,stringsAsFactors = F )
 head(mechacar_df)
 # Perform linear regression
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, mechacar_df)
-
 # Using the summary function to determine the p-value and the r-squared value
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, mechacar_df))  
 
 # p-value: 5.35e-11
 # Multiple R-squared:  0.7149, Adjusted R-squared:  0.6825
 
+
 # Deliverable 2:
-suspensioncoil_table<-read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+# import and read
+sus_df<-read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+# summaries the suspension coil's PSI column
+total_summary<-sus_df |> summarise(mean=mean(PSI), median=median(PSI), variance=var(PSI), sd=sd(PSI))
+# use group by and summaries the suspension coil's PSI column
+lot_summary<-sus_df |> 
+  group_by(Manufacturing_Lot) |> summarise(mean=mean(PSI), median=median(PSI), variance=var(PSI), sd=sd(PSI))
