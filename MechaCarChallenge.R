d2973dac07f0cdd6c5504abbad9dc4c1b7cb6a93
echoqshen <90880564+echoqshen@users.noreply.github.com>
2022-01-09 20:54:59 -0500

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,3 +20,21 @@ total_summary<-sus_df |> summarise(mean=mean(PSI), median=median(PSI), variance=
 # use group by and summaries the suspension coil's PSI column
 lot_summary<-sus_df |> 
   group_by(Manufacturing_Lot) |> summarise(mean=mean(PSI), median=median(PSI), variance=var(PSI), sd=sd(PSI))
+
+
+# Deliverable 3
+# t-test to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(sus_df$PSI, mu=1500)
+# three more t tests
+lot1<-subset(sus_df,Manufacturing_Lot=='Lot1')
+lot2<-subset(sus_df,Manufacturing_Lot=='Lot2')
+lot3<-subset(sus_df,Manufacturing_Lot=='Lot3')
+
+t.test(lot1$PSI, mu=1500)
+# t = 0, df = 49, p-value = 1
+
+t.test(lot2$PSI, mu=1500)
+# t = 0.51745, df = 49, p-value = 0.6072
+
+t.test(lot3$PSI, mu=1500)
+# t = -2.0916, df = 49, p-value = 0.04168
